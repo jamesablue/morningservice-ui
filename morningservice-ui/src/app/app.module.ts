e4c2cd27c770a449a3be5e74875091684598eade
James Blue <blue.j4@gmail.com>
2017-11-22 21:37:09 -0500

    primitave train info station panel. Is not working yet

--- morningservice-ui/src/app/app.module.ts
+++ morningservice-ui/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TrainInfoComponent } from './train-info/train-info.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrainInfoComponent
   ],
   imports: [
     BrowserModule
